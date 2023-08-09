c92d72aec7f92e81e1b89bf6e5f54e6996222cb1
CTfeno98 <69533255+CTfeno98@users.noreply.github.com>
2023-08-09 06:21:00 -0700

    Add files via upload

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTA0MjQwMTUxMTYzMzg2MjY2OQ.GZawHU.jfbLtQ-z9PGPa7N7hruv86nT-dvKlbKuzLQbZ8",  // lrows token
+    PREFIX: process.env.PREFIX || ".", // lrows prefix
+    SAHİP: process.env.SAHİP || "1026780357598662686", //lrows id
+}
